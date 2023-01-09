ad4bb336deb3ae673ea4b7a34791153f66314063
Shrey Shah <85416316+Grap3King@users.noreply.github.com>
2023-01-08 23:23:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,25 @@ TeamInfo = response.json()
 print(TeamInfo)
 TeamKey = TeamInfo['key']
 TeamName = TeamInfo['nickname']
+
 response2 = requests.get('https://www.thebluealliance.com/api/v3/team/' + TeamKey + "/events/2022/keys", headers = headers)
 EventKeysInfo = response2.json() 
+print("\n")
 print(EventKeysInfo)
 
 response3 = requests.get('https://www.thebluealliance.com/api/v3/team/' + TeamKey + "/event/" + EventKeysInfo[1] + "/matches/keys", headers = headers)
 MatchKeysInfo = response3.json()
+print("\n")
 print(MatchKeysInfo)
 
-response4 = requests.get()
+match1 = requests.get('https://www.thebluealliance.com/api/v3/match/' + MatchKeysInfo[1] + "/simple", headers = headers)
+match2 = match1.json()
+alliance = match2['alliances']
+red = alliance['red']
+teams = red['team_keys']
+blue = alliance['blue']
+teams2 = blue['team_keys']
+print("\n")
+print(teams)
+print("\n")
+print(teams2)
